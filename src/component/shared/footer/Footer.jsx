3ef9e8daf849e11730ba27b9f23bb7ca666492bd
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-05-02 17:28:19 +0600

    git ignore fixed

--- src/component/shared/footer/Footer.jsx
+++ src/component/shared/footer/Footer.jsx
@@ -1,11 +1,81 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-    return (
-        <div>
-            <h2>this is footer</h2>
+  return (
+    <footer className="bg-danger text-white mt-5">
+      <div className="container py-3">
+        <div className="row">
+          <div className="col-md-6">
+            <p className="mb-0 fw-bold fs-5">Connect with us:</p>
+            <ul className="list-inline">
+              <li className="list-inline-item">
+                <a
+                  href="#"
+                  className="text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  Facebook
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  href="#"
+                  className="text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  Twitter
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  href="#"
+                  className="text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-6">
+            <p className="mb-0 fw-bold fs-5">Contact us:</p>
+            <ul className="list-inline">
+              <li className="list-inline-item">
+                <a
+                  href="mailto:info@example.com"
+                  className="text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  info@example.com
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  href="#"
+                  className="text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  Send us a message
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  href="#"
+                  className="text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  Privacy Policy
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
